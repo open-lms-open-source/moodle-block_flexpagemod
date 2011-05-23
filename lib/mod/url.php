24521ddccc27848c4830496555db8bb5b73f8b96
Mark Nielsen <mark@moodlerooms.com>
2011-05-23 20:34:17 +0000

    INT-1406: Adding support for mod/url (isn't great though)
    git-svn-id: http://svn.moodlerooms.com/Moodle/2.0/branches/plugins@15102 f529b7b8-409e-e885-f664-9768b4ed42ab

---
+++ lib/mod/url.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Display mod/url
+ */
+class block_flexpagemod_lib_mod_url extends block_flexpagemod_lib_mod {
+    /**
+     * Pretty much copied everything from mod/url/view.php and url_display_embed()
+     *
+     * @return void
+     */
+    public function module_block_setup() {
+        global $CFG, $COURSE, $DB, $PAGE;
+
+        $cm      = $this->get_cm();
+        $url     = $DB->get_record('url', array('id' => $cm->instance));
+        $context = get_context_instance(CONTEXT_MODULE, $cm->id);
+        $course  = $COURSE;
+        if ($url and has_capability('mod/url:view', $context)) {
+            require_once($CFG->dirroot.'/mod/url/locallib.php');
+            require_once($CFG->libdir . '/completionlib.php');
+
+            add_to_log($course->id, 'url', 'view', 'view.php?id='.$cm->id, $url->id, $cm->id);
+
+            // Update 'viewed' state if required by completion system
+            $completion = new completion_info($course);
+            $completion->set_module_viewed($cm);
+
+            $mimetype = resourcelib_guess_url_mimetype($url->externalurl);
+            $fullurl  = url_get_full_url($url, $cm, $course);
+            $title    = $url->name;
+
+            $link = html_writer::tag('a', $fullurl, array('href'=>str_replace('&amp;', '&', $fullurl)));
+            $clicktoopen = get_string('clicktoopen', 'url', $link);
+
+            $extension = resourcelib_get_extension($url->externalurl);
+
+            if (in_array($mimetype, array('image/gif','image/jpeg','image/png'))) {  // It's an image
+                $code = resourcelib_embed_image($fullurl, $title);
+
+            } else if ($mimetype == 'audio/mp3') {
+                // MP3 audio file
+                $code = resourcelib_embed_mp3($fullurl, $title, $clicktoopen);
+
+            } else if ($mimetype == 'video/x-flv' or $extension === 'f4v') {
+                // Flash video file
+                $code = resourcelib_embed_flashvideo($fullurl, $title, $clicktoopen);
+
+            } else if ($mimetype == 'application/x-shockwave-flash') {
+                // Flash file
+                $code = resourcelib_embed_flash($fullurl, $title, $clicktoopen);
+
+            } else if (substr($mimetype, 0, 10) == 'video/x-ms') {
+                // Windows Media Player file
+                $code = resourcelib_embed_mediaplayer($fullurl, $title, $clicktoopen);
+
+            } else if ($mimetype == 'video/quicktime') {
+                // Quicktime file
+                $code = resourcelib_embed_quicktime($fullurl, $title, $clicktoopen);
+
+            } else if ($mimetype == 'video/mpeg') {
+                // Mpeg file
+                $code = resourcelib_embed_mpeg($fullurl, $title, $clicktoopen);
+
+            } else if ($mimetype == 'audio/x-pn-realaudio-plugin') {
+                // RealMedia file
+                $code = resourcelib_embed_real($fullurl, $title, $clicktoopen);
+
+            } else {
+                // anything else - just try object tag enlarged as much as possible
+                // $code = resourcelib_embed_general($fullurl, $title, $clicktoopen, $mimetype);
+                $this->default_block_setup();
+                return;
+            }
+
+            ob_start();
+            url_print_heading($url, $cm, $course);
+            echo $code;
+            url_print_intro($url, $cm, $course);
+            $this->append_content(ob_get_contents());
+            ob_end_clean();
+        }
+    }
+}
