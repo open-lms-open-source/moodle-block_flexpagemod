f1c8ba3e8e75b15094e01b46fc4410039ecaaaef
David Castro <david.castro@blackboard.com>
2018-04-23 15:40:30 +0000

    INT-13192: Importing fi lang

---
+++ lang/fi/block_flexpagemod.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cmdisplayerror'] = 'Liitetyn aktiviteetin näyttäminen epäonnistui. Tarkista lohkon määritykset. Virheilmoitus: {$a}';
+$string['displayactivity'] = 'Näytä aktiviteetti';
+$string['displayactivity_help'] = 'Tässä valittu aktiviteetti näytetään lohkossa.';
+$string['pluginname'] = 'Flexpage-aktiviteetti';
+$string['flexpagemod:addinstance'] = 'Lisää uusi Flexpage-aktiviteettilohko';
