d91aaa5afb4b8feb69b0749b4a64e5d13d6c99e4
David Castro <david.castro@blackboard.com>
2018-04-23 15:30:44 +0000

    INT-13192: Importing ja lang

---
+++ lang/ja/block_flexpagemod.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cmdisplayerror'] = '関連付けられた活動の表示に失敗しました。ブロックの設定を確認してください。エラーメッセージ : "{$a}"';
+$string['displayactivity'] = '活動を表示する';
+$string['displayactivity_help'] = 'ここで選択した活動がブロックに表示されます。';
+$string['pluginname'] = 'Flexpage活動';
+$string['flexpagemod:addinstance'] = '新しいFlexpage活動ブロックを追加する';
