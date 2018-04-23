069a46910da1c0dda8f68471af6c008fe527224c
David Castro <david.castro@blackboard.com>
2018-04-23 15:42:57 +0000

    INT-13192: Importing es lang

---
+++ lang/es/block_flexpagemod.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cmdisplayerror'] = 'Error al mostrar la actividad asociada; confirme los ajustes del bloque. Mensaje de error: "{$a}"';
+$string['displayactivity'] = 'Mostrar actividad';
+$string['displayactivity_help'] = 'La actividad elegida aquí se mostrará en el bloque.';
+$string['pluginname'] = 'Actividad de página Flexpage';
+$string['flexpagemod:addinstance'] = 'Agregar un nuevo bloque de actividad de Flexpage';
