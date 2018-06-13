e67954651f345a3f1c19b3cc55e5dd2e2cdecfa5
David Castro <david.castro@blackboard.com>
2018-06-13 14:04:46 +0000

    INT-13365: Importing tr lang

---
+++ lang/tr/block_flexpagemod.php
@@ -0,0 +1,28 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @copyright  Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['cmdisplayerror'] = 'İlişkili etkinlik görüntülenemedi, lütfen blokun yapılandırmalarını onaylayın. Hata iletisi: "{$a}"';
+$string['displayactivity'] = 'Etkinliği göster';
+$string['displayactivity_help'] = 'Seçilen etkinlik, blok içinde gösterilecek.';
+$string['pluginname'] = 'Flexpage Etkinliği';
+$string['flexpagemod:addinstance'] = 'Yeni bir Flexpage Etkinliği bloku ekle';
